082153e5dabe91ac41c549539082d0ee702d44de
Benjamin Tu <tu.benjamin1115@gmail.com>
2019-06-26 21:31:05 -0700

    try signed

--- signed/src/main/scala/VectorMult.scala
+++ signed/src/main/scala/VectorMult.scala
@@ -3,81 +3,11 @@ package example
 import chisel3._
 import chisel3.util._
 
-class Adder(dataBits: Int = 8) extends Module {
-  val io = IO(new Bundle {
-    val a = Input(SInt(dataBits.W))
-    val b = Input(SInt(dataBits.W))
-    val y = Output(SInt((dataBits+1).W))
-  })  
-  io.y := io.a +& io.b
-}
-
-
-class Multiplier(dataBits: Int = 8) extends Module {
-  val io = IO(new Bundle {
-    val a = Input(SInt(dataBits.W))
-    val b = Input(SInt(dataBits.W))
-    val y = Output(SInt((dataBits*2).W))
-  })
-  io.y := io.a * io.b
-}
-
-class VectorMult(dataBits: Int = 8, vectorLength: Int = 1) extends Module {
-  require(vectorLength > 0)
-	require(dataBits > 0)
-	val io = IO(new Bundle {
- 		val a = Input(Vec(vectorLength, SInt(dataBits.W)))
-		val b = Input(Vec(vectorLength, SInt(dataBits.W)))
-		val y = Output(Vec(vectorLength, SInt((2*dataBits).W)))	 
-	})
-	for (i <- 0 until vectorLength) {
-		io.y(i) := io.a(i) * io.b(i)
-	}
-  when(true.B) {
-    printf("a: ")
-    for (i <- 0 until vectorLength) {
-      printf("%d ,", io.a(i))
-    }   
-    printf("\nb: ")
-    for (j <- 0 until vectorLength) {
-       printf("%d ,", io.b(j))
-    }
-	}
-}
-
-
-class Dot(dataBits: Int = 8, vectorLength: Int = 1) extends Module {
-  require(vectorLength > 0)
-	require(dataBits > 0)
-  val io = IO(new Bundle {
-    val a = Input(Vec(vectorLength, SInt(dataBits.W)))
-    val b = Input(Vec(vectorLength, SInt(dataBits.W)))
-    val y = Output(SInt((2*dataBits + vectorLength-1).W))
-  })
-
-	val initVals = VecInit(Seq.fill(vectorLength){0.S(32.W)})
-  val accum = RegInit(initVals)
-	accum(0) := io.a(0) * io.b(0)
-  for (i <- 1 until vectorLength) {
-      accum(i) := accum(i-1) +& (io.a(i) * io.b(i))
-  }
-  io.y := accum(vectorLength-1)
-}
-
-class Pop(dataBits: Int = 8) extends Module {
-	val io = IO(new Bundle {
-		val num = Input(UInt(dataBits.W))
-		val count = Output(UInt(dataBits.W))	
-	})	
-		io.count := PopCount(io.num)
-}
-
-
 class BitPack(dataBits: Int = 2, vectorLength: Int = 1) extends Module{
 	require(vectorLength > 0)
 	val io = IO(new Bundle {
-		val arr = Input(Vec(vectorLength, UInt(dataBits.W)))
-		val out = Output(Vec(dataBits, UInt(vectorLength.W)))
+		val arr = Input(Vec(vectorLength, SInt(dataBits.W)))
+		val out = Output(Vec(dataBits, SInt(vectorLength.W)))
 	})
 	// LSB is first element
   // e.g. 1010 will be 5
@@ -92,22 +22,22 @@ class BitPack(dataBits: Int = 2, vectorLength: Int = 1) extends Module{
 	}
 	
 	for (k <- 0 until dataBits) {
-		io.out(k) := bitArr(k).asUInt
+		io.out(k) := bitArr(k).asSInt
 	}
 }
 
 
-class BitSerial(wBits: Int = 1, 
-	aBits: Int = 1, vectorLength: Int = 1) extends Module{
+class BitSerial(wBits: Int = 2, 
+	aBits: Int = 2, vectorLength: Int = 1) extends Module{
 	require(vectorLength > 0)
-	require(wBits >= 0 && wBits <= 3)
-	require(aBits >= 0 && aBits <= 3)
+	require(wBits > 0)
+	require(aBits > 0)
 	val io = IO(new Bundle {
-		val weight = Input(Vec(wBits, UInt(vectorLength.W)))
-		val activation = Input(Vec(aBits, UInt(vectorLength.W)))
-		val product = Output(UInt((wBits + aBits + vectorLength-1).W))
+		val weight = Input(Vec(wBits, SInt(vectorLength.W)))
+		val activation = Input(Vec(aBits, SInt(vectorLength.W)))
+		val product = Output(SInt((wBits + aBits + vectorLength-1).W))
 	})
-	val initVal = VecInit(Seq.fill(wBits * aBits)(0.U((wBits + aBits + vectorLength-1).W)))
+	val initVal = VecInit(Seq.fill(wBits * aBits)(0.S((wBits + aBits + vectorLength-1).W)))
 	val sum = RegInit(initVal)
 	sum(0) := PopCount(io.weight(0) & io.activation(0)) 
 	for (i <- 0 until wBits) {
